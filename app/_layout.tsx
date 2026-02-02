cf279ba5d7b6a085393ee36e5668a875baf8b399
Gustavohlma <gustavohlma8@gmail.com>
2026-02-01 21:02:26 -0300

    feat: add Sentry integration and EAS build configuration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,8 +6,28 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { AuthProvider } from '@/contexts/AuthContext';
+import * as Sentry from '@sentry/react-native';
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://3630aa30418784a4a9ff56bf29b99bb7@o4510737328635904.ingest.us.sentry.io/4510737334009856',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -41,5 +61,4 @@ export default function RootLayout() {
       </ThemeProvider>
     </AuthProvider>
   );
-}
-
+});
